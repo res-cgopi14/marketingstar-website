15ced99e9cba180d084efc00a46a82e0a6047b8c
jeeva-res <res.pjeevanantham@gmail.com>
2025-04-04 15:56:46 +0530

    form page changes on field and added consent - Jeeva
    consent field added and validation changes uopdated ,with backend team worked python by sudhager and db check with pradeep,
    
    js validation -  done
    email recived data -  done
    db data consetn -  done

--- assets/js/request-demo.js
+++ assets/js/request-demo.js
@@ -27,14 +27,19 @@ function requestDemoForm(event) {
   var emailExp = /^([A-Za-z0-9_\-\.])+@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
   var firstName = document.getElementById("firstName").value.trim();
-  var lastName = document.getElementById("lastName").value.trim();
+  // var lastName = document.getElementById("lastName").value.trim();
   var emailId = document.getElementById("email").value.trim();
   var mobileNo = document.getElementById("phoneNumber").value.trim();
   var countryCode = document.getElementById("countryCode").value.trim();
   var companyName = document.getElementById("companyName").value.trim();
   var website = document.getElementById("website").value.trim();
-  var termsCheckbox = document.getElementById("termsCheckbox").checked;
-  var consultCheckbox = document.getElementById("consultCheckbox").checked;
+  // var termsCheckbox = document.getElementById("termsCheckbox").checked;
+  var livedemo = document.getElementById("live_demo_req").value;
+  var mailcheckbox = document.getElementById("mailcheckbox").checked; 
+  let mailcheckboxvalue = mailcheckbox?"yes":"no";
+
+
+  // var consultCheckbox = document.getElementById("consultCheckbox").checked;
 
   let isValid = true;
 
@@ -47,14 +52,14 @@ function requestDemoForm(event) {
     document.getElementById("error_firstname").style.display = "none";
   }
 
-  if (lastName === "" || !lastName.match(alphaExp)) {
-    document.getElementById("error_lastname").innerHTML =
-      "Enter last name";
-    document.getElementById("error_lastname").style.display = "block";
-    isValid = false;
-  } else {
-    document.getElementById("error_lastname").style.display = "none";
-  }
+  // if (lastName === "" || !lastName.match(alphaExp)) {
+  //   document.getElementById("error_lastname").innerHTML =
+  //     "Enter last name";
+  //   document.getElementById("error_lastname").style.display = "block";
+  //   isValid = false;
+  // } else {
+  //   document.getElementById("error_lastname").style.display = "none";
+  // }
 
   if (emailId === "" || !emailId.match(emailExp)) {
     document.getElementById("error_emailid").innerHTML =
@@ -91,15 +96,34 @@ function requestDemoForm(event) {
     document.getElementById("error_website").style.display = "none";
   }
 
-  if (!termsCheckbox) {
-    document.getElementById("error_checkbox").innerHTML =
-      "You must agree to the terms";
-    document.getElementById("error_checkbox").style.display = "block";
+  
+ 
+  if (livedemo === "") {
+    document.getElementById("error_demo_request").innerHTML = "Please select Yes or No";
+    document.getElementById("error_demo_request").style.display = "block";
     isValid = false;
   } else {
-    document.getElementById("error_checkbox").style.display = "none";
+    document.getElementById("error_demo_request").style.display = "none";
   }
 
+  
+//  if (!mailcheckbox) {
+//     document.getElementById("error_checkbox").innerHTML =
+//       "Required";
+//     document.getElementById("error_checkbox").style.display = "block";
+//     isValid = false;
+//   } else {
+//     document.getElementById("error_checkbox").style.display = "none";
+//   }
+
+  // if (!termsCheckbox) {
+  //   document.getElementById("error_checkbox").innerHTML =
+  //     "You must agree to the terms";
+  //   document.getElementById("error_checkbox").style.display = "block";
+  //   isValid = false;
+  // } else {
+  //   document.getElementById("error_checkbox").style.display = "none";
+  // }
   if (isValid) {
     var formattedMobileNo = `(+${countryCode}) ${mobileNo}`;
     // let formData = {
@@ -113,22 +137,43 @@ function requestDemoForm(event) {
     //   "Subject": "New Demo Request Submission"
     // };
     let formData = {
-      "First_name": firstName,
-      "Last_name": lastName,
-      "EmailID": emailId,
-      "MobileNo": formattedMobileNo,
-      "Website_Domain": website,
-      "Company": companyName,
-      "Request_demo": consultCheckbox,
-      "Subject": "New Demo Request Submission",
-      "formid": "229",
-      "apikey": "cc33cd4b_2fea_4b94_9123_bb7d48ff673e",
-      "SourceURL": "",
-      "pagereferrerurl": "",
-      "rid": "",
-      "cid": "",
-      "pagetitle": ""
+      // "First_name": firstName,
+      // // "Last_name": lastName,
+      // "EmailID": emailId,
+      // "MobileNo": formattedMobileNo,
+      // "Website_Domain": website,
+      // "Company": companyName,
+      // // "Request_demo": consultCheckbox,
+      // // "Request_demo": livedemo,
+      // "Live_Domain":livedemo,
+      // "checkbox":mailcheckboxvalue,
+      // "Subject": "New Demo Request Submission",
+      // "formid": "229",
+      // "apikey": "cc33cd4b_2fea_4b94_9123_bb7d48ff673e",
+      // "SourceURL": "",
+      // "pagereferrerurl": "",
+      // "rid": "",
+      // "cid": "",
+      // "pagetitle": ""
+
+        "TextBlock":"",
+        "Name":firstName,
+        "MobileNo":formattedMobileNo,
+        "EmailID":emailId,
+        "Website_Domain":website,
+        "Company":companyName,
+        "Live_Domain":livedemo,
+        "Consent":mailcheckboxvalue,
+        "Subject": "New Demo Request Submission",
+        "formid":"232",
+        "apikey":"cc33cd4b_2fea_4b94_9123_bb7d48ff673e",
+        "SourceURL":"",
+        "pagereferrerurl":"",
+        "rid":"",
+        "cid":"",
+        "pagetitle":""
     }
+    // console.log(formData)
     // fetch("https://resu.io/Subscription/IndexInsertTemp/cust_cc33cd4b_2fea_4b94_9123_bb7d48ff673e/229", {
     const urls = [
         'https://resu.io/Subscription/IndexInsertAPI',
@@ -148,7 +193,7 @@ function requestDemoForm(event) {
         const dataPromises = responses.map(response => response.json());
         const data = await Promise.all(dataPromises);
     
-        console.log(data);
+        // console.log(data);
 
         demoModal.hide();
 
